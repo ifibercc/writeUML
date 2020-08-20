67f7c222af9f8d89dcb2a0c3d90379e0abe6f4dd
jingchu.fhb <jingchu.fhb@alibaba-inc.com>
2020-08-20 14:28:46 +0800

    样式 功能点优化

---
+++ app/components/DiagramTpl.ts
@@ -0,0 +1,82 @@
+export const flowchart = `graph TD;
+  A-->B;
+  A-->C;
+  B-->D;
+  C-->D;
+`;
+
+export const sequenceDiagram = `sequenceDiagram
+  participant Alice
+  participant Bob
+  Alice->>John: Hello John, how are you?
+  loop Healthcheck
+      John->>John: Fight against hypochondria
+  end
+  Note right of John: Rational thoughts <br/>prevail!
+  John-->>Alice: Great!
+  John->>Bob: How about you?
+  Bob-->>John: Jolly good!
+`;
+
+export const ganttDiagram = `gantt
+  dateFormat  YYYY-MM-DD
+  title Adding GANTT diagram to mermaid
+  excludes weekdays 2014-01-10
+
+  section A section
+  Completed task            :done,    des1, 2014-01-06,2014-01-08
+  Active task               :active,  des2, 2014-01-09, 3d
+  Future task               :         des3, after des2, 5d
+  Future task2               :         des4, after des3, 5d
+`;
+
+export const classDiagram = `classDiagram
+  Class01 <|-- AveryLongClass : Cool
+  Class03 *-- Class04
+  Class05 o-- Class06
+  Class07 .. Class08
+  Class09 --> C2 : Where am i?
+  Class09 --* C3
+  Class09 --|> Class07
+  Class07 : equals()
+  Class07 : Object[] elementData
+  Class01 : size()
+  Class01 : int chimp
+  Class01 : int gorilla
+  Class08 <--> C2: Cool label
+`;
+
+export const gitGraph = `gitGraph:
+options
+{
+    "nodeSpacing": 150,
+    "nodeRadius": 10
+}
+end
+commit
+branch newbranch
+checkout newbranch
+commit
+commit
+checkout master
+commit
+commit
+merge newbranch
+`;
+
+export const entityRelationshipDiagram = `erDiagram
+  CUSTOMER ||--o{ ORDER : places
+  ORDER ||--|{ LINE-ITEM : contains
+  CUSTOMER }|..|{ DELIVERY-ADDRESS : uses
+`;
+
+export const userJourneyDiagram = `journey
+  title My working day
+  section Go to work
+    Make tea: 5: Me
+    Go upstairs: 3: Me
+    Do work: 1: Me, Cat
+  section Go home
+    Go downstairs: 5: Me
+    Sit down: 5: Me
+`;
--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,36 +1,74 @@
 import React, { useState } from 'react';
+import { shell } from 'electron';
 import mermaid from 'mermaid';
 import AceEditor from 'react-ace';
-import 'ace-builds/src-noconflict/mode-markdown';
-import 'ace-builds/src-noconflict/theme-monokai';
 import _ from 'lodash';
 import { saveAs } from 'file-saver';
-import { Button, Space } from 'antd';
+import { Button, Space, Dropdown, Menu } from 'antd';
+import { DownloadOutlined, BarsOutlined } from '@ant-design/icons';
+import 'ace-builds/src-noconflict/mode-markdown';
+import 'ace-builds/src-noconflict/theme-monokai';
+
+import * as diagramTpl from './DiagramTpl';
 
 import styles from './Home.css';
 
+const { SubMenu } = Menu;
+
 export default function Home(): JSX.Element {
-  const [mermaidSVG, setMermaidSVG] = useState();
+  const [mermaidSVG, setMermaidSVG] = useState<string>('');
+  const [code, setCode] = useState<string>('');
 
   const renderMermaid = _.debounce((code: string) => {
-    mermaid.mermaidAPI.render('mermaid', code, (svg: BlobPart) => {
+    mermaid.mermaidAPI.render('mermaid', code, (svg) => {
+      setCode(code);
       setMermaidSVG(svg);
     });
   }, 1000);
 
   const handleClickDownload = () => {
-    const blob = new Blob([mermaidSVG], { type: 'image/svg+xml;charset=utf-8' });
+    const blob = new Blob([mermaidSVG], {
+      type: 'image/svg+xml;charset=utf-8',
+    });
     saveAs(blob, 'uml.svg');
   };
+
+  const handleClickHelperMenu = ({ key }: { key: string }) => {
+    if (key === 'helperDoc') {
+      shell.openExternal(
+        'https://mermaid-js.github.io/mermaid/#/?id=diagrams-that-mermaid-can-render'
+      );
+    } else {
+      renderMermaid(diagramTpl[key]);
+    }
+  };
+
+  const menu = (
+    <Menu theme="dark" onClick={handleClickHelperMenu}>
+      <SubMenu title="插入模板">
+        <Menu.Item key="flowchart">Flowchart</Menu.Item>
+        <Menu.Item key="sequenceDiagram">Sequence Diagram</Menu.Item>
+        <Menu.Item key="ganttDiagram">Gantt Diagram</Menu.Item>
+        <Menu.Item key="classDiagram">Class Diagram</Menu.Item>
+        <Menu.Item key="gitGraph">Git Graph</Menu.Item>
+        <Menu.Item key="entityRelationshipDiagram">
+          Entity Relationship Diagram
+        </Menu.Item>
+        <Menu.Item key="userJourneyDiagram">User Journey Diagram</Menu.Item>
+      </SubMenu>
+      <Menu.Item key="helperDoc">帮助文档</Menu.Item>
+    </Menu>
+  );
 
   return (
     <div className={styles.container} data-tid="container">
       <AceEditor
         mode="markdown"
         theme="monokai"
+        value={code}
         onChange={renderMermaid}
         className={styles.editor}
-        width="50vw"
+        width="512px"
         height="100vh"
         editorProps={{ $blockScrolling: true }}
         fontSize={14}
@@ -49,8 +87,17 @@ export default function Home(): JSX.Element {
         className={styles.mermaid}
         dangerouslySetInnerHTML={{ __html: mermaidSVG }}
       />
+      <Space className={styles.helper}>
+        <Dropdown overlay={menu} placement="topLeft" arrow>
+          <Button icon={<BarsOutlined />} shape="circle" type="primary" />
+        </Dropdown>
+      </Space>
       <Space className={styles.operation}>
-        <Button onClick={handleClickDownload}>下载</Button>
+        <Button
+          onClick={handleClickDownload}
+          icon={<DownloadOutlined />}
+          shape="circle"
+        />
       </Space>
     </div>
   );
